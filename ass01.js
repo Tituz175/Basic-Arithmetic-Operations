fdb807cc7e40ce8030ad1c6c7d0c43d108d15d13
Tobi Oyekanmi <tobititus01@gmail.com>
2020-12-10 17:33:20 +0100

    work done and all files added

---
+++ ass01.js
@@ -0,0 +1,31 @@
+function rate() {
+    var num01 = Number(document.getElementById("num1").value)
+    var num02 = Number(document.getElementById("num2").value)
+    var act = document.getElementById("opera").value
+    var show
+
+    if (act == "+" || act == "plus") {
+        show = num01 + num02
+    }
+    else if (act == "-" || act == "subtract") {
+        show = num01 - num02
+    }
+    else if (act == "/" || act == "divide") {
+        show = num01 / num02
+    }
+    else if (act == "*" || act == "multiply") {
+        show = num01 * num02
+    }
+    else if (act == "%" || act == "reminder") {
+        show = num01 % num02
+    }
+    else if (act == "^" || act == "power") {
+        show = num01 ** num02
+    }
+    else if (act == "root") {
+        var nu = 1 / num02
+        console.log(nu)
+        show = num01 ** nu
+    }
+    document.getElementById("screen").innerHTML = show
+}
